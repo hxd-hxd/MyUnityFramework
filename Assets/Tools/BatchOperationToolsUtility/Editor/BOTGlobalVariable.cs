4d311dbc7d5dc40e5bbd67387df8fdcc19da80bb
hxd-hxd <404987771@qq.com>
2024-03-28 09:46:50 +0800

    批量操作工具修改： 1、修改文件结构 2、添加Rolsyn编译器库

---
+++ Assets/Tools/BatchOperationToolsUtility/Editor/BOTGlobalVariable.cs
@@ -0,0 +1,37 @@
+﻿// -------------------------
+// 创建日期：2021/8/4 14:10:32
+// -------------------------
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace BatchOperationToolsUtility
+{
+    /// <summary>
+    /// 全局变量
+    /// </summary>
+    public static class BOTGlobalVariable
+    {
+        /// <summary>
+        /// 要修改成的目标命名空间
+        /// </summary>
+        public static string namespaceValue;
+        /// <summary>
+        /// 是否修改
+        /// </summary>
+        public static bool modify = false;
+        /// <summary>
+        /// 是否添加
+        /// </summary>
+        public static bool add = true;
+
+        /// <summary>
+        /// 要操作的项列表
+        /// </summary>
+        public static List<OperationItem> namespaceFileList = new List<OperationItem>();
+
+    }
+}
---
+++ Assets/Tools/BatchOperationToolsUtility/Editor/BOTRoslyn.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Microsoft.CSharp;
+//using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+
+namespace BatchOperationToolsUtility
+{
+    public class BOTRoslyn
+    {
+        //Microsoft.CodeAnalysis.CSharp.CSharpSyntaxTree
+        
+        void Teat()
+        {
+            var st = CSharpSyntaxTree.ParseText("");
+        }
+    }
+}
