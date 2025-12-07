05370b8ed23b675ef2b11136947159706e8bd522
hxd-hxd <404987771@qq.com>
2025-12-07 22:31:00 +0800

    添加：测试代码 修改：1、升级YooAsset 2、更新框架

---
+++ Assets/Test/Scripts/TestUpdateDeltaTime.cs
@@ -0,0 +1,32 @@
+﻿// -------------------------
+// 创建日期：2025/12/7 22:01:34
+// -------------------------
+
+using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Test
+{
+    public class TestUpdateDeltaTime : MonoBehaviour
+    {
+        public int targetFrameRate = 60;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            Application.targetFrameRate = targetFrameRate;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            Debug.Log($"当前帧率：{Application.targetFrameRate}，单帧时间：{Time.deltaTime}，不缩放的单帧时间：{Time.unscaledDeltaTime}");
+
+            Application.targetFrameRate = targetFrameRate;
+
+        }
+    }
+}
